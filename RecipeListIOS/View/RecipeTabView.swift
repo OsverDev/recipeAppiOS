da298d77aaa206e7875af734053657069bcddf10
Osver Fernandez <osverdev@icloud.com>
2022-07-06 22:38:31 -0600

    Enter tabView code general structure

--- RecipeListIOS/View/RecipeTabView.swift
+++ RecipeListIOS/View/RecipeTabView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct RecipeTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        TabView{
+            VStack{
+                Text("Featured View")
+            }
+            .tabItem{
+                Image(systemName: "star")
+                Text("Featured")
+            }
+            RecipeListView()
+                .tabItem(){
+                    Image(systemName: "list.bullet")
+                    Text("List")
+                }
+        }
     }
 }
 
